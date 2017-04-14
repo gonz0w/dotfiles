93d09ce49b890ca00cddf0af3c01717b33e0e470
gonz0w <cam@fungalo.co>
2017-04-14 11:48:52 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 mv .* ~
-sudo yum install etags-ctags figlet vim lsof htop bash-completion tcpdump -y
+sudo yum install wget etags-ctags figlet vim lsof htop bash-completion tcpdump -y
 source ~/.bashrc
 bind -f ~/.inputrc
+cd ~/.vim
+mkdir autoload
+wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 
 
 
