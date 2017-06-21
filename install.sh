39e143286aab33b92d784846a4070096f1f8f14e
gonz0w <cam@fungalo.co>
2017-06-21 13:30:32 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,10 @@ cp -r .vim ~
 cp .vimrc ~
 cp .lessfilter ~
 sudo yum install wget etags-ctags figlet vim lsof htop bash-completion telnet nc psmisc tcpdump bind-utils -y
-source ~/.bashrc
-bind -f ~/.inputrc
 cd ~/.vim
 mkdir autoload && cd autoload
 wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 cd ~
 chmod +x .lessfilter
-rm -rf dotfiles
-
-
-
+echo ' please run this command : source ~/.bashrc && bind -f ~/.inputrc '
