50b318ef46f76c294d9244b4c16ef4b769733b4f
gonz0w <cam@fungalo.co>
2017-12-19 11:33:41 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp .inputrc ~
 cp -r .vim ~
 cp .vimrc ~
 cp .lessfilter ~
-sudo yum install wget etags-ctags figlet vim lsof htop bash-completion telnet nc psmisc tcpdump bind-utils nodejs -y
+sudo yum install unzip wget etags-ctags figlet vim lsof htop bash-completion telnet nc psmisc tcpdump bind-utils nodejs -y
 wget https://github.com/beautify-web/js-beautify/archive/master.zip && unzip master.zip && cp -rf js-beautify-master/js ~/.vim/plugin/lib/
 cd ~/.vim
 mkdir autoload && cd autoload
