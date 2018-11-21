219e3aae54d385cebe3a176549704099ba514a65
gonz0w <cam@fungalo.co>
2018-11-21 11:13:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.
 sudo yum install ccze jq unzip pip wget etags-ctags figlet vim lsof htop bash-completion telnet nc psmisc tcpdump bind-utils nodejs npm jq -y
 sudo npm install -g jshint
 wget https://github.com/beautify-web/js-beautify/archive/master.zip && unzip master.zip && cp -rf js-beautify-master/js ~/.vim/plugin/lib/
-wget https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/jsshell-linux-i686.zip
+wget https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/jsshell-linux-x86_64.zip
 unzip jsshell-linux-i686.zip
 mv js /usr/local/bin
-mv *.dylib /usr/local/bin
+mv *.so /usr/local/bin
 pip install mycli
 rm -rf js*.zip
 cd ~/.vim
